1b5764b3aeae65d27b9e7717d672ae032fdeab44
coramdeo59 <melkamuelias59@gmail.com>
2024-10-24 15:53:17 +0300

    add component

---
+++ src/app/components/logo.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <div className="text-xl font-bold">foodZone</div>
+  )
+}
+export default Logo;
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import AppHeader from "../components/app-header";
+import BackgroundPattern from "../components/background-pattern";
 
 const Layout = () => {
   return (
     <div>
-     
-
+      <BackgroundPattern  />
+      <AppHeader />
+      
     </div>
   )
 }
